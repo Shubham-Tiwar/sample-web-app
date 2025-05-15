59e316455367151ec62a66a1fde5f269d0e276db
Shubham-Tiwar <shubham.tiwari28@gmail.com>
2025-05-15 15:30:41 +0530

    change in java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,7 +13,7 @@ public class DemoApplication {
 
     @GetMapping("/")
     public String home() {
-        return "✅ Hello from Spring Boot deployed via Jenkins!";
+        return "✅ Hello from Spring Boot deployed via Jenkins in AWS!";
     }
 }
 
