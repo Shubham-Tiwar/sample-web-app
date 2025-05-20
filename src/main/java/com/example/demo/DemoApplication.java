7e7e7c36904c1d979dfd57ca424d4cbffad2793f
Shubham-Tiwar <shubham.tiwari28@gmail.com>
2025-05-20 10:43:52 +0530

    Update jenkinsfile

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,7 +13,7 @@ public class DemoApplication {
 
     @GetMapping("/")
     public String home() {
-        return "✅ Hello from Spring-Boot java app deployed !";
+        return "✅ Hello from Spring-Boot java app deployed from VM !";
     }
 }
 
